d96899bbd234756fd37ea50f49f3fe32532b3d60
Iliash Petro <iliashpp@dtek.com>
2024-09-24 16:26:49 +0300

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import routes from '@/constants/routes';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect(`/surveys/test-survey`);
+  redirect(routes.TEST_SURVEY);
 }
